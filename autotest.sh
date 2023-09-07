306a8ad7f72688c0796a51b764e67d6319d380d1
Oleg G.Kapranov <ok@pure-agency.co>
2023-09-06 20:39:05 -1000

    added script for test - autotest.sh

---
+++ autotest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ "$1" = "--umbrella" ] || [ "$1" = "-u" ]
+then
+  while true; do
+    inotifywait -r -e modify,move,create,delete apps/ && mix test --include integration
+  done
+else
+  while true; do
+    inotifywait -r -e modify,move,create,delete apps/core/lib/ apps/core/test/ && mix test --include integration
+  done
+fi
