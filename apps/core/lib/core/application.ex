62dcc7aaa409772446fa54dd6c7714fc71f72197
Oleg G.Kapranov <ok@pure-agency.co>
2023-09-10 03:36:02 -1000

    added Core.Config, Core.Context, Core.Model, Core.Seeder.Repo

--- apps/core/lib/core/application.ex
+++ apps/core/lib/core/application.ex
@@ -3,7 +3,8 @@ defmodule Core.Application do
 
   use Application
 
-  @impl true
+  @spec start(Application.start_type(), start_args :: term()) ::
+          {:ok, pid()} | {:ok, pid(), Application.state()} | {:error, reason :: term()}
   def start(_type, _args) do
     children = [Core.Repo]
     opts = [strategy: :one_for_one, name: Core.Supervisor]
---
+++ apps/core/lib/core/seeder/repo.ex
@@ -0,0 +1,20 @@
+defmodule Core.Seeder.Repo do
+  @moduledoc """
+  Seeds for `Core.Seeder.Repo` repository.
+  """
+
+  @spec seed!() :: :ok
+  def seed! do
+    :ok
+  end
+
+  @spec updated!() :: :ok
+  def updated! do
+    :ok
+  end
+
+  @spec deleted!() :: :ok
+  def deleted! do
+    :ok
+  end
+end
