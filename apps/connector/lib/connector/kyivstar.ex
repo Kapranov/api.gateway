cd12524dc8ae4cd99394af33c736ae823d60dd72
Oleg G.Kapranov <ok@pure-agency.co>
2023-10-27 02:40:11 -1000

    updated Connector

--- apps/connector/lib/connector/kyivstar.ex
+++ apps/connector/lib/connector/kyivstar.ex
@@ -3,6 +3,8 @@ defmodule Connector.Kyivstar do
   Kyivstar REST API wrapper in Elixir
   """
 
+  alias Connector.Utils
+
   use Tesla
   plug Tesla.Middleware.BaseUrl, "http://httpbin.org/"
   plug Tesla.Middleware.Headers, [{"content-type", "application/json"}]
@@ -11,31 +13,29 @@ defmodule Connector.Kyivstar do
   @message_body "You will beat yourself up if you sell this unit!  I sold mine for a Tak and hate every minute of it."
   @phone_number "+380991111111"
 
+  @doc """
+  Virtual Kyivstar REST API
+
+  ## Example
+
+      iex> Connector.Kyivstar.send(%{phone_number: "+380991111111", message_body: "Hello World!"})
+      {:ok, %{"status" => "send"}}
+      iex> Connector.Kyivstar.send(%{phone_number: "+380991111111", message_body: "Hello World!"})
+      {:ok, %{"status" => "error"}}
+
+  """
+  @spec send(%{phone_number: String.t(), message_body: String.t()}) :: {:ok, %{String.t() => String.t()}}
   def send(args \\ %{phone_number: @phone_number, message_body: @message_body}) do
     path = "post"
-    case random_state() do
+    case Utils.random_state() do
       :ok ->
         data = %{"status" => "send", "sms" => args.phone_number, "text" => args.message_body}
         {:ok, %Tesla.Env{:body => body}} = post(path, data)
-        {:ok, transfer(body["data"])}
+        {:ok, Utils.transfer(body["data"])}
       :error ->
         data = %{"status" => "error"}
         {:ok, %Tesla.Env{:body => body}} = post(path, data)
-        {:ok, transfer(body["data"])}
+        {:ok, Utils.transfer(body["data"])}
     end
-  end
-
-  @spec random_state() :: atom()
-  defp random_state do
-    value = ~W(ok error)a
-    Enum.random(value)
-  end
-
-  @spec transfer(map()) :: map()
-  defp transfer(data) do
-    data
-    |> Jason.decode!
-    |> Map.delete("sms")
-    |> Map.delete("text")
   end
 end
---
+++ apps/connector/lib/connector/utils.ex
@@ -0,0 +1,19 @@
+defmodule Connector.Utils do
+  @moduledoc """
+  Some function for Connector.
+  """
+
+  @spec random_state() :: atom()
+  def random_state do
+    value = ~W(ok error)a
+    Enum.random(value)
+  end
+
+  @spec transfer(map()) :: map()
+  def transfer(data) do
+    data
+    |> Jason.decode!
+    |> Map.delete("sms")
+    |> Map.delete("text")
+  end
+end
