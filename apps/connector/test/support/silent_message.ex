f0605a561fae8fe7e2c1b886a20ab8a7d8b1c876
Oleg G.Kapranov <ok@pure-agency.co>
2023-12-13 22:41:53 -1000

    updated spec and docs for SilentMessage

--- apps/connector/test/support/silent_message.ex
+++ apps/connector/test/support/silent_message.ex
@@ -1,4 +1,28 @@
 defmodule SilentMessage do
+  @moduledoc """
+  TestBrod call the message handler with the restructured Kafka message.
+  """
+
+  @connector :kafka
+
   def handle_message(_message), do: :ok
-  def handle_messages(_message), do: :ok
+
+  def handle_messages(messages) do
+    for %{key: key, value: value} = message <- messages do
+      case is_list(:ets.info(@connector)) do
+        true ->
+          try do
+            :ets.insert(@connector, {key, value})
+          rescue
+            ArgumentError ->
+              IO.inspect message
+          end
+        false ->
+          :ets.new(@connector, [:set, :public, :named_table])
+          :ets.insert(@connector, {key, value})
+      end
+      IO.inspect message
+    end
+    :ok
+  end
 end
