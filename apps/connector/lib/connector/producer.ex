30582d24e525614b4535050a11af26f90f802081
Oleg G.Kapranov <ok@pure-agency.co>
2023-12-13 22:36:45 -1000

    updated spec and docs for Connector.Producer

--- apps/connector/lib/connector/producer.ex
+++ apps/connector/lib/connector/producer.ex
@@ -13,37 +13,25 @@ defmodule Connector.Producer do
   ## Example.
 
       iex> args = %{id: "AcV6bF6mODc3JIsGf2", phone_number: "+380997170609", message_body: "Ваш код - 7777-999-9999-9999"}
-      iex> Connector.Producer.send(1, args)
+      iex> Connector.Producer.runner(args)
       :ok
       iex>[data] = :ets.lookup(:kafka, "AcV6bF6mODc3JIsGf2")
-      {"AcV6bF6mODc3JIsGf2", "+380997170609, Ваш код - 7777-999-9999-9999"}
+      [{"AcV6bF6mODc3JIsGf2", "{\"status\":\"send\",\"text\":\"Ваш код - 7777-999-9999-9999\",\"connector\":\"vodafone\",\"sms\":\"+380997170609\",\"ts\":1702542761151}"}]
       iex> data |> elem(0)
       "AcV6bF6mODc3JIsGf2"
       iex> data |> elem(1)
-      "+380997170609, Ваш код - 7777-999-9999-9999"
+      "{\"status\":\"send\",\"text\":\"Ваш код - 7777-999-9999-9999\",\"connector\":\"vodafone\",\"sms\":\"+380997170609\",\"ts\":1702542761151}"
 
       iex> :ets.lookup_element(:kafka, "AcV6bF6mODc3JIsGf2", 1)
       "AcV6bF6mODc3JIsGf2"
       iex> :ets.lookup_element(:kafka, "AcV6bF6mODc3JIsGf2", 2)
-      "+380997170609, Ваш код - 7777-999-9999-9999"
+      "{\"status\":\"send\",\"text\":\"Ваш код - 7777-999-9999-9999\",\"connector\":\"vodafone\",\"sms\":\"+380997170609\",\"ts\":1702542761151}"
 
       iex> :ets.lookup_element(:kafka, "AcV6bF6mODc3JIsGf2", 2) |> String.split(", ")
-      ["+380997170609", "Ваш код - 7777-999-9999-9999"]
+      ["{\"status\":\"send\",\"text\":\"Ваш код - 7777-999-9999-9999\",\"connector\":\"vodafone\",\"sms\":\"+380997170609\",\"ts\":1702542761151}"]
 
       iex> :ets.delete(:kafka, "AcV6bF6mODc3JIsGf2")
       true
-
-      iex> key = ""
-      iex> value = ~s({"a":"#{UUID.uuid4()}","timestamp":#{:os.system_time(:milli_seconds)},"c":"aBcDeFgHiJk","d":"This is a test","e":"#{UUID.uuid4()}","f":"testing"})
-      iex> value = ~s({"id":"Ac7y2LxiD9lsV2Oeiu","status":"send","text":"Ваш код - 7777-999-9999-9999 - vodafone","connector":"vodafone","sms":"+380991111111","timestamp":#{:os.system_time(:milli_seconds)}})
-      iex> value |> Jason.decode!
-      iex> id = "Ac7y2LxiD9lsV2Oeiu"
-      iex> messages = ~s({"status":"send","text":"Ваш код - 7777-999-9999-9999 - vodafone","connector":"vodafone","sms":"+380991111111"})
-      iex> Kaffe.Producer.produce_sync(id, messages)
-      iex> run_start = :os.system_time(:milli_seconds)
-      iex> KaffeTestProducer.runner(messages_count)
-      iex> run_end = :os.system_time(:milli_seconds)
-      iex> messages_sec = (messages_count / (run_end - run_start))*1000
 
   """
   @spec runner(map()) :: map()
@@ -51,11 +39,13 @@ defmodule Connector.Producer do
 
   @spec send_message(integer(), map()) :: :ok
   defp send_message(n, args) do
+    key = "#{args.id}"
     t_start = :os.system_time(:milli_seconds)
+    value = ~s({"status":"send","text":"#{args.message_body}","connector":"vodafone","sms":"#{args.phone_number}","ts":#{:os.system_time(:milli_seconds)}})
 
     if n > 0 do
       try do
-        Kaffe.Producer.produce_sync("#{args.id}", "#{args.phone_number}, #{args.message_body}")
+        Kaffe.Producer.produce_sync(key, value)
       rescue
         e -> e
       catch
